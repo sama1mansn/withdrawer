888325964ebf7a1a5907a762c9606d2c2c0aaa50
sama1mansn <136932204+sama1mansn@users.noreply.github.com>
2023-11-24 14:52:23 +0330

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
+	"github.com/ethereum-optimism/optimism/op-bindings/bindings""context"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
